5e47f44cf25c01fb52279943c88f4b6cd772b639
fadydieaa22 <fadydieaa22@gmail.com>
2026-01-24 02:20:53 +0200

    change url in cors2

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL
       ? process.env.CLIENT_URL.split(",").map((url) => url.trim())
-      : ["http://localhost:5173", "http://localhost:5174"],
+      : ["https://devconnect-xi-woad.vercel.app/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
